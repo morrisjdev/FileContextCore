3bf344789a281531e679aaab0ab9d78004c85971
Morris Janatzek <morrisj@live.de>
2019-10-30 10:54:40 +0100

    fixed license command

--- FileContextCore/Diagnostics/FileContextEventId.cs
+++ FileContextCore/Diagnostics/FileContextEventId.cs
@@ -1,4 +1,6 @@
-// Copyright (c) morrisjdev & .NET Foundation. All rights reserved.
+// Copyright (c) morrisjdev. All rights reserved.
+// Original copyright (c) .NET Foundation. All rights reserved.
+// Modified version by morrisjdev
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.Diagnostics;
