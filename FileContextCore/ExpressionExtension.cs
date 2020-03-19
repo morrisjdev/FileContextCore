a7d1acfae129c1042fa7fee25064004547b3f4f1
morrisjdev <morrisj@live.de>
2020-03-19 12:32:39 +0100

    update to ef core 3.1.2

---
+++ FileContextCore/ExpressionExtension.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+namespace FileContextCore
+{
+    public static class ExpressionExtension
+    {
+        public static LambdaExpression UnwrapLambdaFromQuote(this Expression expression)
+            => (LambdaExpression)(expression is UnaryExpression unary && expression.NodeType == ExpressionType.Quote
+                ? unary.Operand
+                : expression);
+    }
+}
